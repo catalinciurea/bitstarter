a24b08378e5f30d38f5f8f7b51d7c7c739415213
Catalin Ciurea <catalin.ciurea@microchip.com>
2013-07-08 23:03:43 +0300

    Modified the app to serve a statuc html file

--- web.js
+++ web.js
@@ -5,8 +5,16 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+app.set('views', __dirname);
+app.engine('html', require('ejs').renderFile);
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+//  response.send('Hello World!');
+//    var body = "Hello World";
+//    response.setHeader('Content-Type','text/plain');
+//    response.setHeader('Content-Length', body.length);
+//    response.end(body);
+    response.render('index.html')
 });
 
 var port = process.env.PORT || 5000;
